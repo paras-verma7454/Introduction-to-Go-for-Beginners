a0785135ac76794f0d1068d53edf5fe9c16de545
paras-verma7454 <parasverma7454@gmail.com>
2025-02-14 17:27:33 +0530

    Chapter 2

--- slices/main.go
+++ slices/main.go
@@ -6,21 +6,23 @@ import (
 )
 
 // We will be modifying this later
-func CreateSlice() []int{
-	slice1:= []int{1, 2, 3, 4, 5}
+func CreateSlice() []string{
+	slice1:= []string{"dog","cat","monkey"}
 	return slice1
 }
 
-func ModifySlice() []int{
-	slice2:= []int{1, 2, 3, 4, 5}
-	slice2= append(slice2, 6)
-	return slice2
+func ModifySlice(slice []string) []string{
+	if len(slice) <3{
+		return slice
+	}
+	slice[1] = "elephant"
+	
 }
 
-func PopSliceValue() []int{
-	slice3:= []int{1, 2, 3, 4, 5}
-	slice3 = slices.Delete(slice3, 2,4)
-	return slice3
+func PopSliceValue(slice []string) []string{
+	slice= slices.Delete(slice, 2,4)
+	return slice
+
 }
 
 // Where we will run our code
@@ -34,11 +36,12 @@ func main() {
 	// 	fmt.Printf("color %s at index %d\n", color, i)
 	// }
 	
-	// oldSlice:=[]string{"apple", "carrot","cheese"}
-	// oldSlice = slices.Delete(oldSlice,0,1)
-	// fmt.Println(oldSlice)
+	oldSlice:=[]string{"apple", "carrot","cheese"}
+	oldSlice = slices.Delete(oldSlice,0,1)
+	fmt.Println(oldSlice)
 
 	fmt.Println(CreateSlice())
-	fmt.Println(ModifySlice())
-	fmt.Println(PopSliceValue())
+	slice1:= CreateSlice()
+	fmt.Println(ModifySlice(slice1))
+	fmt.Println(PopSliceValue(slice1))
 }
