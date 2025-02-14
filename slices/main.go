00fc2a43d7e707fb5830b11b186e4fdcc6028189
paras-verma7454 <parasverma7454@gmail.com>
2025-02-14 16:56:57 +0530

    Chapter 2

--- slices/main.go
+++ slices/main.go
@@ -1,11 +1,47 @@
 package main
 
+import (
+	"fmt"
+)
+
 // We will be modifying this later
-func CreateSlice() {}
+func CreateSlice() []int{
+	slice:= []int{1, 2, 3, 4, 5}
+	return slice
+}
 
-func ModifySlice() {}
+func ModifySlice() []int{
+	slice:= []int{1, 2, 3, 4, 5}
+	slice= append(slice, 6)
+	return slice
+}
 
-func PopSliceValue() {}
+func PopSliceValue() int{
+	slice:= []int{1, 2, 3, 4, 5}
+	return slice[0]
+}
 
 // Where we will run our code
-func main() {}
+func main() {
+	// numbersArray:= []int{}
+	// var realEmptySlice []int
+
+	// if len(numbersArray) == 0 {
+	// 	fmt.Println("numbersArray is empty")
+	// }
+	// if len(realEmptySlice) == 0 {
+	// 	fmt.Println("realEmptySlice is empty")
+	// }
+
+	// myColors:= []string{"red", "green", "yellow"}
+	// // fmt.Println("myColors:", myColors[1])
+	// myColors= append(myColors, "blue")
+	// // fmt.Println("myColors:", myColors[3])
+
+	// for i, color := range myColors{
+	// 	fmt.Printf("color %s at index %d\n", color, i)
+	// }
+	fmt.Println(CreateSlice())
+	fmt.Println(ModifySlice())
+	fmt.Println(PopSliceValue())
+}
