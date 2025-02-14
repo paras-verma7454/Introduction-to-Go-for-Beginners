a90ce768258e287382ede8f46d5f1dba518f4485
paras-verma7454 <parasverma7454@gmail.com>
2025-02-14 17:11:00 +0530

    Chapter 2

--- slices/main.go
+++ slices/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"slices"
 )
 
 // We will be modifying this later
@@ -10,37 +11,33 @@ func CreateSlice() []int{
 	return slice1
 }
 
-func ModifySlice() int{
+func ModifySlice() []int{
 	slice2:= []int{1, 2, 3, 4, 5}
 	slice2= append(slice2, 6)
-	return slice2[5]
+	return slice2
 }
 
-func PopSliceValue() int{
+func PopSliceValue() []int{
 	slice3:= []int{1, 2, 3, 4, 5}
-	return slice3[0]
+	slice3 = slices.Delete(slice3, 2,4)
+	return slice3
 }
 
 // Where we will run our code
 func main() {
-	// numbersArray:= []int{}
-	// var realEmptySlice []int
-
-	// if len(numbersArray) == 0 {
-	// 	fmt.Println("numbersArray is empty")
-	// }
-	// if len(realEmptySlice) == 0 {
-	// 	fmt.Println("realEmptySlice is empty")
-	// }
-
 	// myColors:= []string{"red", "green", "yellow"}
 	// // fmt.Println("myColors:", myColors[1])
 	// myColors= append(myColors, "blue")
-	// // fmt.Println("myColors:", myColors[3])
+	// fmt.Println("myColors:", myColors[3])
 
 	// for i, color := range myColors{
 	// 	fmt.Printf("color %s at index %d\n", color, i)
 	// }
+	
+	// oldSlice:=[]string{"apple", "carrot","cheese"}
+	// oldSlice = slices.Delete(oldSlice,0,1)
+	// fmt.Println(oldSlice)
+
 	fmt.Println(CreateSlice())
 	fmt.Println(ModifySlice())
 	fmt.Println(PopSliceValue())
