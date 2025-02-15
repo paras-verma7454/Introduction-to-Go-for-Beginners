ee3da767cc50d442d48c911e153f01781784d58b
paras-verma7454 <parasverma7454@gmail.com>
2025-02-15 16:41:27 +0530

    Chapter 4

--- structs/main.go
+++ structs/main.go
@@ -3,18 +3,27 @@ package main
 import "fmt"
 
 type Person struct {
-	Name 		string
-	Age  		int
-	Employed 	bool
+	Name     string
+	Age      int
+	Employed bool
 }
-// We will be modifying this later
-func UpdateNameByValue(person Person, name string) Person{
+
+// Function to create a new Person
+func NewPerson(name string, age int, employed bool) Person {
+	return Person{
+		Name:     name,
+		Age:      age,
+		Employed: employed,
+	}
+}
+
+func UpdateNameByValue(person Person, name string) Person {
 	person.Name = name
 	return person
 }
 
-func UpdateNameByReference(Person *Person, name string) {
-	Person.Name = name
+func UpdateNameByReference(person *Person, name string) {
+	person.Name = name
 }
 
 func MakePersonEmployed(person *Person) {
@@ -23,13 +32,9 @@ func MakePersonEmployed(person *Person) {
 
 // Where we will run our code
 func main() {
-	Person1 := Person{
-		Name: "John",
-		Age: 25,
-		Employed: false,
-	}
-	fmt.Println(UpdateNameByValue(Person1,"Jane"))
+	Person1 := NewPerson("John", 25, false)
+	fmt.Println(UpdateNameByValue(Person1, "Jane"))
 	UpdateNameByReference(&Person1, "Jony")
 	MakePersonEmployed(&Person1)
-
+	fmt.Println(Person1)
 }
