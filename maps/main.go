01c3f2d5915dee5a3b9dbc7743d1ea1bc78db259
paras-verma7454 <parasverma7454@gmail.com>
2025-02-15 14:52:26 +0530

    Chapter 3

--- maps/main.go
+++ maps/main.go
@@ -1,11 +1,37 @@
 package main
 
+import (
+	"fmt"
+)
+
 // We will be modifying this later
-func CreateMap() {}
+func CreateMap() map[string]string {
+	color := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+		"white": "#ffffff",
+	}
+	return color
+}
 
-func AddToMap() {}
+func AddToMap(colors map[string]string) map[string]string{
+	colors["blue"] = "#0000ff"
+	return colors
+}
 
-func DeleteFromMap() {}
+func DeleteFromMap(colors map[string]string) map[string]string{
+	delete(colors, "blue")
+	return colors
+}
 
 // Where we will run our code
-func main() {}
+func main() {
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+		"white": "#ffffff",
+	}
+	fmt.Println(CreateMap())
+	fmt.Println(AddToMap(colors))
+	fmt.Println(DeleteFromMap(colors))
+}
